2af9616edf64cd4843763159d3c89e980dce8dbb
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-11-12 12:40:50 +0100

    Update Palo.kt
    Kdoc documentation added
    
    -----------
    
    Añadida documentación kdoc

--- app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Palo.kt
+++ app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Palo.kt
@@ -2,6 +2,9 @@
 
 package com.arodmar432p.a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar
 
+/**
+ * Enum class que representa los palos de una baraja.
+ */
 enum class Palo {
     CORAZONES,
     DIAMANTES,
