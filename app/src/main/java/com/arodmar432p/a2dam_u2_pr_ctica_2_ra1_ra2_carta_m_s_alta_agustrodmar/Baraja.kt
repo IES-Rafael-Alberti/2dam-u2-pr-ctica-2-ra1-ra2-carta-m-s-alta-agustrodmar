119b90daeb9e85a8c1c983616913ad2dac297343
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-11-12 12:38:43 +0100

    Update Baraja.kt
    kdoc documentation and comments added.
    
    -----
    
    Añadida documentación Kdoc y comentarios.

--- app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Baraja.kt
+++ app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Baraja.kt
@@ -6,15 +6,29 @@ import android.annotation.SuppressLint
 import android.content.Context
 
 
+/**
+ * Clase Baraja que representa una baraja de cartas.
+ */
 object Baraja {
+    // Lista para almacenar las cartas de la baraja
     private val listaCartas = ArrayList<Carta>()
 
+    /**
+     * Función para crear una baraja de cartas.
+     *
+     * @param context Contexto de la aplicación.
+     */
     @SuppressLint("DiscouragedApi")
     fun crearBaraja(context: Context) {
+        // Itera sobre todos los palos
         for (palo in Palo.values()) {
+            // Itera sobre todos los naipes
             for (naipe in Naipe.values()) {
+                // Determina los puntos mínimos y máximos de la carta
                 val puntosMin = if (naipe == Naipe.AS) 1 else naipe.valor
                 val puntosMax = if (naipe == Naipe.AS) 11 else naipe.valor
+
+                // Determina el nombre del recurso de la carta
                 val nombreRecurso = when (naipe) {
                     Naipe.AS -> "${palo.toString().lowercase()}a"
                     Naipe.DOS -> "${palo.toString().lowercase()}2"
@@ -30,16 +44,28 @@ object Baraja {
                     Naipe.DAME -> "${palo.toString().lowercase()}q"
                     Naipe.ROI -> "${palo.toString().lowercase()}k"
                 }
-                val idDrawable = context.resources.getIdentifier(nombreRecurso, "drawable", context.packageName)
-                listaCartas.add(Carta(naipe, palo, puntosMin, puntosMax, idDrawable))
 
+                // Obtiene el identificador del recurso drawable
+                val idDrawable = context.resources.getIdentifier(nombreRecurso, "drawable", context.packageName)
+
+                // Añade la carta a la lista de cartas
+                listaCartas.add(Carta(naipe, palo, puntosMin, puntosMax, idDrawable))
             }
         }
     }
+
+    /**
+     * Función para barajar las cartas.
+     */
     fun barajar() {
         listaCartas.shuffle()
     }
 
+    /**
+     * Función para obtener una carta de la baraja.
+     *
+     * @return Carta La carta obtenida de la baraja.
+     */
     fun dameCarta(): Carta {
         return listaCartas.removeAt(listaCartas.size - 1)
     }
