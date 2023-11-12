32394ec569cb9728100ec2d2082ba1a19a23f593
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-11-12 12:42:26 +0100

    Update MainActivity.kt
    I’ve created a user interface that displays a card and two buttons, “Dame Carta” and “Reiniciar”.
    
    The card initially displayed is face down. When the user clicks on “Dame Carta”, a card is dealt from the deck and displayed. When the user clicks on “Reiniciar”, a new deck is created, shuffled, and the card is displayed face down again.
    
    -------------------------------------------
    
    He creado una interfaz de usuario que muestra una carta y dos botones, "Dame Carta" y "Reiniciar". 
    
    La carta mostrada inicialmente está boca abajo. Cuando el usuario hace clic en "Dame Carta", se reparte una carta de la baraja y se muestra. Cuando el usuario hace clic en "Reiniciar", se crea una nueva baraja, se baraja y se vuelve a mostrar la carta boca abajo.

--- app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/MainActivity.kt
+++ app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/MainActivity.kt
@@ -30,7 +30,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arodmar432p.a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar.ui.theme._2damu2prctica2ra1ra2cartamsaltaagustrodmarTheme
 
+
+**
+ * Representa la actividad principal de la aplicación.
+ */
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Función que se llama al crear la actividad.
+     * @param savedInstanceState Bundle que contiene el estado de la actividad.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -47,33 +56,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Función composable que representa el juego de BlackJack.
+ */
 @SuppressLint("DiscouragedApi")
 @Preview
 @Composable
 fun JuegoBlackJack() {
+    // Obtener el contexto actual
     val context = LocalContext.current
+
+    // Estado para la carta actual
     var cartaActual by remember { mutableStateOf(Carta(Naipe.AS, Palo.CORAZONES, 1, 11, 0)) }
+
+    // Estado para mostrar la carta
     var mostrarCarta by remember { mutableStateOf(false) }
 
+    // Crear y barajar la baraja
     Baraja.crearBaraja(context)
     Baraja.barajar()
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        if (mostrarCarta) {
-            val idDrawable = context.resources.getIdentifier(
-                "${cartaActual.palo.toString().lowercase()}_${cartaActual.nombre.toString().lowercase()}",
-                "drawable",
-                context.packageName
-            )
-            Image(painter = painterResource(id = idDrawable), contentDescription = null)
+    // Diseño de la interfaz de usuario
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Determina el id del recurso drawable
+        val idDrawable = if (mostrarCarta) {
+            cartaActual.idDrawable
         } else {
-            val idDrawable = context.resources.getIdentifier("carta_boca_abajo", "drawable", context.packageName)
-            Image(painter = painterResource(id = idDrawable), contentDescription = null)
+            context.resources.getIdentifier("bocabajo", "drawable", context.packageName)
         }
+
+        // Muestra la imagen de la carta
+        Image(painter = painterResource(id = idDrawable), contentDescription = null)
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Row {
+            // Botón para obtener una carta
             Button(onClick = {
                 cartaActual = Baraja.dameCarta()
                 mostrarCarta = true
@@ -83,6 +105,7 @@ fun JuegoBlackJack() {
 
             Spacer(modifier = Modifier.width(8.dp))
 
+            // Botón para reiniciar el juego
             Button(onClick = {
                 Baraja.crearBaraja(context)
                 Baraja.barajar()
