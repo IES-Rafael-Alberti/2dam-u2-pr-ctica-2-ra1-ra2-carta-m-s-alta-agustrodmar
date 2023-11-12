4d499fcde91b188223e948f116af336e2ba963e1
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-11-12 12:39:25 +0100

    Update Carta.kt
    Kdoc documentation and comments added.
    
    --------
    
    Añadida documentación kdoc y comentarios.

--- app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Carta.kt
+++ app/src/main/java/com/arodmar432p/a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar/Carta.kt
@@ -2,6 +2,15 @@
 
 package com.arodmar432p.a2dam_u2_pr_ctica_2_ra1_ra2_carta_m_s_alta_agustrodmar
 
+/**
+ * Representa una carta de baraja.
+ *
+ * @property nombre El nombre del naipe de la carta.
+ * @property palo El palo de la carta.
+ * @property puntosMin Los puntos mínimos que puede tener la carta.
+ * @property puntosMax Los puntos máximos que puede tener la carta.
+ * @property idDrawable El identificador del recurso drawable de la carta.
+ */
 data class Carta(
     val nombre: Naipe,
     val palo: Palo,
